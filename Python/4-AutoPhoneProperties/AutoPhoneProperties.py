fcadb02ba05b007417b29ed0fffc9b9ac583ca65
Maksym Melanchenko <maksym.melanchenko@harman.com>
2018-01-19 10:00:00 +0100

    added PATH for linux; \t replacement

--- Python/4-AutoPhoneProperties/AutoPhoneProperties.py
+++ Python/4-AutoPhoneProperties/AutoPhoneProperties.py
@@ -4,15 +4,17 @@ import re
 
 KNOWN_PHONES = ['CB5A27NUU4','10160ad8efee3403','0915f983de722d01','05445efb0a5cb40f','09a7ad48029a8b12','05535d970a5cab52','069a23bb344b6eaa',
 '02b6f328d0236113','079ec7be00d355d8','32045110369281ad','ce0716079de52a1702','11160be17a933201','W3D7N15616010803','4790c0ccaeaa10fe']
-PATH = "D:\\TestApplicationPhone.properties"
+
+#PATH = "D:\\TestApplicationPhone.properties"
+PATH = "/home/maksym/project/TestController/resources/TestApplicationPhone.properties"
 PHONES_SEARCH_EXPRESSION = re.compile("phone.\d")
 
 actualPhones = []
 errorPhones = []
 
 print "\nRunning 'adb devices'...\n"
-comResult = (subprocess.check_output(["testInput.py"],shell=True)).decode("utf-8")
-modifStr = comResult.replace("\r", ",").replace(" ", ",").replace("\n", ",")
+comResult = (subprocess.check_output(["adb devices"],shell=True)).decode("utf-8")
+modifStr = comResult.replace("\r", ",").replace(" ", ",").replace("\n", ",").replace("\t", ",")
 elements = modifStr.split(',')
 
 for x in range (0, len(elements)) :
