31c1dd06b1cdcb5ed4feac5032016177a70e9d27
maksym.melanchenko <maksym.melanchenko@pragmaticplay.com>
2021-05-12 14:19:52 +0300

    input delay time

--- Python/7-pb-currency/runner.py
+++ Python/7-pb-currency/runner.py
@@ -1,9 +1,11 @@
+import sys
+
 import pb_currency
 import time
 
 from plyer import notification
 
-for i in range(10):
+for i in range(8):
     currencies = pb_currency.prepare_currencies_list()
     stringed_currencies = "".join(currency.get_info() for currency in currencies)
     stringed_currencies = currencies[0].date + "\n" + stringed_currencies
@@ -12,4 +14,4 @@ for i in range(10):
                         app_name="PB_RATES",
                         app_icon="symbol.ico",
                         timeout=10)
-    time.sleep(3600)
+    time.sleep(int(sys.argv[1]))
